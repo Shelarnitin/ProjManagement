ef26433dfe877a79afce5f767088df7b6f322b75
Shelarnitin <shelarnitin399@gmail.com>
2025-08-21 20:04:09 +0530

    User securely logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { registerUser, login} from "../controllers/auth.controllers.js";
+import { registerUser, login, logoutUser} from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(),
 validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post( logoutUser);
 
 export default router;
